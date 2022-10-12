039b46a754f7009f236cabda4188980b83ef347d
kangJia <ikangjia.cn@outlook.com>
2022-10-12 09:45:24 +0800

    [init] 修改登录页面

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,73 @@
 <template>
-<h1>登录页面!!!</h1>
+  <div id="container">
+    <div class="login-box">
+      <h1>登录</h1>
+      <el-form :model="loginForm" size="large" class="form-box">
+        <el-form-item>
+          <el-input v-model="loginForm.account"/>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.password" type="password"/>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width: 100%;" type="primary" @click="doLogin">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
+import {ElMessage} from "element-plus";
+import userApi from '../utils/api/user.js'
 export default {
-  name: "LoginView"
+  name: "LoginView",
+  data() {
+    return {
+      loginForm: {
+        account: 'admin',
+        password: 'admin'
+      }
+    }
+  },
+  methods: {
+    doLogin() {
+      userApi.doLogin(this.loginForm)
+          .then(res => {
+            if (res.code === 0) {
+              window.localStorage.setItem('token', res.data)
+              this.$router.push('/dataSource')
+              ElMessage.success('登录成功～')
+            }
+          })
+    },
+  }
 }
 </script>
 
 <style scoped>
+#container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
+#container .login-box {
+  width: 350px;
+  height: 350px;
+  border-radius: 10px;
+  border: 1px solid lightgray;
+}
+
+#container .login-box h1 {
+  text-align: center;
+  margin-top: 12px;
+  margin-bottom: 15px;
+}
+
+#container .login-box .form-box {
+  padding-left: 20px;
+  padding-right: 20px;
+}
 </style>

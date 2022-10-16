84d535407ed9c4030a2c7db0b31f19d93c0632a0
kangjia <ikangjia.cn@outlook.com>
2022-10-16 12:08:07 +0800

    [feat] 引入 Pinia 状态管理组件

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 import Router from "./router/index.js";
+import {createPinia} from "pinia";
 
+const pinia = createPinia()
 const app = createApp(App)
-app.use(ElementPlus, {size: 'small', zIndex: 3000})
 
+app.use(ElementPlus, {size: 'small', zIndex: 3000})
 app.use(Router)
+app.use(pinia)
 
 // .mount() 方法应该始终在整个应用配置和资源注册完成后被调用
 app.mount('#app')
